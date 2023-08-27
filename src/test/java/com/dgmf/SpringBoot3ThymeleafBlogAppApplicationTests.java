0eb48f7cb0f5f746103e3d09ddb06e0862bb90dc
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-08-27 06:53:41 +0200

    Project Initialization

---
+++ src/test/java/com/dgmf/SpringBoot3ThymeleafBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3ThymeleafBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
