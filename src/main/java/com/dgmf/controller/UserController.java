e2408c308b51930cd3d3d70f22f836b933dbced6
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-10-16 12:41:00 +0200

    Thymeleaf Looping or Iteration - th:each Attribute

--- src/main/java/com/dgmf/controller/UserController.java
+++ src/main/java/com/dgmf/controller/UserController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class UserController {
     // Handler Method for the Http
@@ -41,14 +44,14 @@ public class UserController {
         return "selection-expression";
     }
 
-    // Handler Method to handler Message Expression Request
+    // Handler Method to handle Message Expression Request
     // Request "http://localhost:8080/message-expression"
     @GetMapping("/message-expression")
     public String messageExpression() {
         return "message-expression";
     }
 
-    // Handler Method to handler Link Expression Request
+    // Handler Method to handle Link Expression Request
     // Request "http://localhost:8080/link-expression"
     @GetMapping("/link-expression")
     public String linkExpression(Model model) {
@@ -57,10 +60,46 @@ public class UserController {
         return "link-expression";
     }
 
-    // Handler Method to handler Fragment Expression Request
+    // Handler Method to handle Fragment Expression Request
     // Request "http://localhost:8080/fragment-expression"
     @GetMapping("/fragment-expression")
     public String fragmentExpression(Model model) {
         return "fragment-expression";
+    }
+
+    // Handler Method to handle Looping or Iteration in 
+    // Thymeleaf (th:each Attribute)
+    // Request "http://localhost:8080/users"
+    @GetMapping("/users")
+    public String users(Model model) {
+        User admin = User.builder()
+                .name("John Doe")
+                .email("johndoe@Gmail.com")
+                .gender("Male")
+                .role("Admin")
+                .build();
+
+        User ovelie = User.builder()
+                .name("Ovelie Ortega")
+                .email("ovelieortega@Gmail.com")
+                .gender("Female")
+                .role("USER")
+                .build();
+
+        User gaspard = User.builder()
+                .name("Gaspard Delafontaine")
+                .email("gasparddelafontaine@Gmail.com")
+                .gender("Male")
+                .role("MANAGER")
+                .build();
+
+        List<User> users = new ArrayList<>();
+        users.add(admin);
+        users.add(ovelie);
+        users.add(gaspard);
+
+        model.addAttribute("users", users);
+
+        return "users";
     }
 }
