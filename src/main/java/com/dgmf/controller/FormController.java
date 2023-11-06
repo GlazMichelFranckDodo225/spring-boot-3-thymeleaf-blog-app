c28d744b97bc0753aa1010203495430cc1285844
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-06 07:52:23 +0100

    Form Handling in Thymeleaf - Display User Registration Form Data

--- src/main/java/com/dgmf/controller/FormController.java
+++ src/main/java/com/dgmf/controller/FormController.java
@@ -4,6 +4,8 @@ import com.dgmf.model.UserForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Arrays;
 import java.util.List;
@@ -29,4 +31,15 @@ public class FormController {
         return "register-form";
     }
 
+    // Handler Method to handle User Registration Form Submission Request
+    //  "http://localhost:8080/register/save"
+    @PostMapping("/register/save")
+    public String submitForm(
+            Model model,
+            @ModelAttribute("userForm") UserForm userForm
+        ) {
+        model.addAttribute("userForm", userForm);
+
+        return "register-success";
+    }
 }
