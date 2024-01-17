f054f0ed29f3bd3be4aa618d3dbd4d0fe2589007
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-01-17 09:19:15 +0100

    View Blog Post Implementation

--- src/main/java/com/dgmf/web/controller/BlogController.java
+++ src/main/java/com/dgmf/web/controller/BlogController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -22,5 +23,18 @@ public class BlogController {
         model.addAttribute("allPosts", allPostDtos);
 
         return "blog/view_posts";
+    }
+
+    // Handler Method for View Post Request
+    // Example: http://localhost:8080/post/
+    @GetMapping("/post/{postUrl}")
+    public String showPost(
+            @PathVariable("postUrl") String postUrl,
+            Model model
+    ) {
+        PostDto postDto = postService.findPostByUrl(postUrl);
+        model.addAttribute("post", postDto);
+
+        return "blog/blog_post";
     }
 }
